b8472142c569b3ee2dc15966ddb0037766fab858
Sergey Toy <toydestroyer@me.com>
2014-11-10 11:58:52 +0300

    Legacy version

---
+++ main.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+    var md = false;
+    var er = false;
+    document.getElementById('eraser').onclick = function(){
+        console.log("er")
+        er = er ? false : true;
+    }
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var ctx2 = document.getElementById('canvas2').getContext('2d');
+    var ctx3 = document.getElementById('canvas3').getContext('2d');
+    var img = new Image();
+    img.src = 'jobs.jpg';
+    img.onload = function(){
+        ctx2.drawImage(img,0,0);
+        ctx.drawImage(Pixastic.process(img, "mosaic", {blockSize:10}),0,0);
+    }
+    window.onmousedown = function(){
+        md = true;
+    }
+    window.onmouseup = function(){
+        md = false;
+    }
+    window.onmousemove = function(e) {
+        if (md) {
+            if(er){
+                ctx3.clearRect(e.pageX-20,e.pageY-20,40,40);
+            }else{
+                ctx3.putImageData(ctx.getImageData(e.pageX-20,e.pageY-20,40,40),e.pageX-20,e.pageY-20);
+            }
+        }
+    }
+}
