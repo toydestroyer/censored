837bef4e26e30b0b4f51dc56faa432eafe062d1a
Sergey Toy <toydestroyer@me.com>
2014-11-22 10:39:29 +0300

    remove all pixelate plugins

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
-window.onload = function () {
-    var md = false;
-    var er = false;
-    document.getElementById('eraser').onclick = function(){
-        console.log("er")
-        er = er ? false : true;
-    }
-    var ctx = document.getElementById('canvas').getContext('2d');
-    var ctx2 = document.getElementById('canvas2').getContext('2d');
-    var ctx3 = document.getElementById('canvas3').getContext('2d');
-    var img = new Image();
-    img.src = 'jobs.jpg';
-    img.onload = function(){
-        ctx.drawImage(img,0,0);
-        ctx2.drawImage(img,0,0);
-        P = new Pixastic(ctx);
-        P['mosaic']({blockSize : 8}).done(function() {
+window.onload = function() {
+  var md = false;
+  var er = false;
 
-        });
-        // ctx.drawImage(Pixastic.process(img, "mosaic", {blockSize:10}),0,0);
+  document.getElementById('eraser').onclick = function() {
+    er = er ? false : true;
+  }
+
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var ctx2 = document.getElementById('canvas2').getContext('2d');
+  var ctx3 = document.getElementById('canvas3').getContext('2d');
+  var img = new Image();
+  img.src = 'jobs.jpg';
+  img.onload = function() {
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+    ctx2.drawImage(img, 0, 0);
+    w = canvas.width * 0.1,
+    h = canvas.height * 0.1;
+    ctx.drawImage(img, 0, 0, w, h);
+    ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
+  }
+  window.onmousedown = function() {
+    md = true;
+  }
+  window.onmouseup = function() {
+    md = false;
+  }
+  window.onmousemove = function(e) {
+    if (md) {
+      if (er) {
+        ctx3.clearRect(e.pageX - 20, e.pageY - 20, 40, 40);
+      } else {
+        ctx3.putImageData(ctx.getImageData(e.pageX - 20, e.pageY - 20, 40, 40), e.pageX - 20, e.pageY - 20);
+      }
     }
-    window.onmousedown = function(){
-        md = true;
-    }
-    window.onmouseup = function(){
-        md = false;
-    }
-    window.onmousemove = function(e) {
-        if (md) {
-            if(er){
-                ctx3.clearRect(e.pageX-20,e.pageY-20,40,40);
-            }else{
-                ctx3.putImageData(ctx.getImageData(e.pageX-20,e.pageY-20,40,40),e.pageX-20,e.pageY-20);
-            }
-        }
-    }
+  }
 }
